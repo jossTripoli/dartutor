79e6f43db0f8076ead1c9daad12b040edd30b7b1
Brandon Arriaga <baa325@lehigh.edu>
2023-09-06 19:04:05 -0400

    added segment with interests and animation

--- public/js/brandon.js
+++ public/js/brandon.js
@@ -1,10 +1,8 @@
-$(document).ready(function() {
-    $('#showProfile').on('click', function() {
-        // Toggle the visibility classes to trigger the transition
-        $('#hideProfile').toggleClass('hidden visible');
+$(document).ready(function () {
+  $("#showProfile").on("click", function () {
+    // Toggle the visibility classes to trigger the transition
+    $("#hideProfile").toggleClass("hidden visible");
 
-        $('#hideProfile')
-            .transition('bounce')
-        ;
-    });
+    $("#hideProfile").transition("tada");
+  });
 });
