ffdce8345f5315dc64c0de576e2240e5feb52ea6
Caden Wright <cjw224@lehigh.edu>
2023-09-13 19:08:07 -0400

    fixed functionality

--- public/js/caden-week2.js
+++ public/js/caden-week2.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
     // displays yes question
     $('#showYes').on('click', function() {
+        if($('#hideNo').hasClass('visible')) {
+            $('#hideNo').toggleClass('hidden visible');
+        }
         // Toggle the visibility classes
         $('#hideYes').toggleClass('hidden visible');
     });
     //displays no question
     $('#showNo').on('click', function() {
+        if($('#hideYes').hasClass('visible')) {
+            $('#hideYes').toggleClass('hidden visible');
+        }
         // Toggle the visibility classes
         $('#hideNo').toggleClass('hidden visible');
     });
