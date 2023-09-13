0316a5a74b04b516caadb7ee6edab35b3b54a77d
Caden Wright <cjw224@lehigh.edu>
2023-09-13 13:33:33 -0400

    added small amount of functionality

---
+++ public/js/caden-week2.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // goes to yes question page
+    $('#showYes').on('click', function() {
+        window.location.href = "/people/caden/reflect2"
+    });
+    // goes to no question page
+    $('#showNo').on('click', function() {
+        window.location.href = "/people/caden/reflect3"
+    });
+});
